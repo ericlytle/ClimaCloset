02ee45fe2430be96a60d8a9e375fa2ece8f0efa9
Eric Lytle <elytle2009@gmail.com>
2016-02-20 21:19:24 -0600

    New activities

---
+++ app/src/main/java/umkc/elmp7/climacloset/ClimaClothesActivites/MainActivity.java
@@ -0,0 +1,82 @@
+package umkc.elmp7.climacloset.ClimaClothesActivites;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import junit.framework.Assert;
+
+import umkc.elmp7.climacloset.OpenWeather.OpenWeatherService;
+import umkc.elmp7.climacloset.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+    private static final String DEFAULTCITYNAME = "Kansas%20City,MO";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button catalogTopButton = (Button) findViewById(R.id.catalogTopButton);
+        Button catalogbottomButton = (Button) findViewById(R.id.catalogBottomButton);
+        EditText cityStateName = (EditText) findViewById(R.id.cityState);
+        cityStateName.setText(DEFAULTCITYNAME);
+
+        new FetchWeatherTask().execute(cityStateName.getText().toString());
+
+        catalogTopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent catalogTopIntent = new Intent(getApplicationContext(), CatalogTopActivity.class);
+                startActivity(catalogTopIntent);
+            }
+        });
+
+        catalogbottomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent catalogBottomIntent = new Intent(getApplicationContext(), CatalogBottomActivity.class);
+                startActivity(catalogBottomIntent);
+            }
+        });
+    }
+
+    private class FetchWeatherTask extends AsyncTask<String, Void, String> {
+
+        // Not sure what the three dots mean? See: http://stackoverflow.com/questions/3158730/java-3-dots-in-parameters?rq=1
+        protected String doInBackground(String... parms) {
+
+            // parms[0] is first parm, etc.
+            OpenWeatherService weatherService = new OpenWeatherService(parms[0]);
+            try {
+                String temperature = weatherService.getTemperature();
+                return temperature;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return "Not available";
+        }
+
+        protected void onProgressUpdate(Void... values) {
+
+        }
+
+        //  invoked on the UI thread after the background computation finishes
+        protected void onPostExecute(String temperature) {
+            // Example assert statements
+            //Assert.assertNull(temperature);
+            Assert.assertNotNull("Error: temperature is null", temperature);
+            Assert.assertTrue(3 < 4);
+
+            TextView tempDisp = (TextView) findViewById(R.id.temp);
+            tempDisp.setText(temperature);
+            //updateUI(temperature);
+        }
+    }
+}
---
+++ app/src/main/java/umkc/elmp7/climacloset/JSON/JSONService.java
@@ -0,0 +1,87 @@
+package umkc.elmp7.climacloset.JSON;
+
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class JSONService {
+
+    private static final String TAG = JSONService.class.getName();
+
+    public static JSONObject getJSONObject(String uri) throws JSONException {
+        return new JSONObject(getJSONString(uri));
+    }
+
+    public static String getJSONString(String uri) {
+
+        StringBuilder builder = new StringBuilder();
+        try {
+
+            URL url = new URL(uri);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.connect();
+            int response = conn.getResponseCode();
+
+            if (response == 200) {
+                InputStream content = conn.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                }
+            } else {
+                Log.e(TAG, "Failed to download file");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }
+
+//    public static String getJSONStringold(String uri) {
+//
+//        StringBuilder builder = new StringBuilder();
+//        HttpClient client = new DefaultHttpClient();
+//        HttpGet httpGet = new HttpGet(uri);
+//        try {
+//            HttpResponse response = client.execute(httpGet);
+//            StatusLine statusLine = response.getStatusLine();
+//            int statusCode = statusLine.getStatusCode();
+//            if (statusCode == 200) {
+//                HttpEntity entity = response.getEntity();
+//                InputStream content = entity.getContent();
+//                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+//                String line;
+//                while ((line = reader.readLine()) != null) {
+//                    builder.append(line);
+//                }
+//            } else {
+//                Log.e(TAG, "Failed to download file");
+//            }
+//        } catch (ClientProtocolException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return builder.toString();
+//    }
+}
